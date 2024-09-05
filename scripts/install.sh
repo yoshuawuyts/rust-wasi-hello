7cef0e8b2d97afd2a7d30d6a7045f6ad404632a8
Aaron Wislang <aaron.wislang@microsoft.com>
2024-09-05 16:52:46 -0400

    Move cargo install cargo-component to install.sh, pin to 0.16.0
    Signed-off-by: Aaron Wislang <aaron.wislang@microsoft.com>

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,3 +7,6 @@ git clone https://github.com/bytecodealliance/wasm-pkg-tools
 cd wasm-pkg-tools/crates/wkg
 cargo install --path .
 cd ../../../../
+
+# install cargo-component
+cargo install cargo-component --version 0.16.0
