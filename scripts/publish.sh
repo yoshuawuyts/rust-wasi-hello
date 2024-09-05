a6bc8bae1eca1b27cfd6db3df50c0f598da05799
Yosh <github@yosh.is>
2024-09-05 18:32:04 +0200

    username

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -10,7 +10,7 @@ cd wasm-pkg-tools
 cargo install --path .
 
 # login to the registry
-gh auth token | docker login ghcr.io --password-stdin
+gh auth token | docker login ghcr.io --username yoshuawuyts --password-stdin
 
 # publish using wkg
 PROJECT_NAME="rust_wasi_hello"
