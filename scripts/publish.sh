6be0038230c0219ec9d845b34c295ef81d55815d
Aaron Wislang <aaron.wislang@microsoft.com>
2024-09-05 16:41:15 -0400

    Merge updated and tested action
    Signed-off-by: Aaron Wislang <aaron.wislang@microsoft.com>

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-# login to the registry
-# if running locally, we may need to: 
-# gh auth refresh -h github.com -s write:packages,read:packages
-gh auth token | docker login ghcr.io --username YOURUSERNAME --password-stdin
-
-# publish using wkg
-PROJECT_NAME="rust_wasi_hello"
-
-if [ -z "$IMAGE_NAME" ]; then
-  GH_USER=$(gh api user --jq '.login')
-  IMAGE_NAME="${GH_USER}/rust-wasi-hello"
-  export IMAGE_NAME
+# not running in github actions? use gh cli to login, etc, locally
+if [ "$GITHUB_ACTIONS" != "true" ]; then
+    # login to the registry
+    # if running locally, we may need to: 
+    # gh auth refresh -h github.com -s write:packages,read:packages
+    gh auth token | docker login ghcr.io --username YOURUSERNAME --password-stdin
+    GH_USER=$(gh api user --jq '.login')
+    IMAGE_NAME="${GH_USER}/rust-wasi-hello"
 fi
 
+PROJECT_NAME="rust_wasi_hello"
 REGISTRY_REFERENCE="ghcr.io/${IMAGE_NAME}:latest"
 
 wkg oci push $REGISTRY_REFERENCE target/wasm32-wasip1/release/$PROJECT_NAME.wasm
