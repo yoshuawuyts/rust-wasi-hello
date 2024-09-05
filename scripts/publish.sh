b96c1ada26ed842136f45259a008b266b9daef52
Aaron Wislang <aaron.wislang@microsoft.com>
2024-09-05 14:31:44 -0400

    Fix wkg installation, update gh auth for local use, include GH_USER
    Signed-off-by: Aaron Wislang <aaron.wislang@microsoft.com>

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -6,13 +6,18 @@ cd tmp
 
 # install wkg
 git clone https://github.com/bytecodealliance/wasm-pkg-tools
-cd wasm-pkg-tools
+cd wasm-pkg-tools/crates/wkg
 cargo install --path .
+cd ../../../../
 
 # login to the registry
-gh auth token | docker login ghcr.io --username yoshuawuyts --password-stdin
+# if running locally, we may need to: 
+# gh auth refresh -h github.com -s write:packages,read:packages
+gh auth token | docker login ghcr.io --username YOURUSERNAME --password-stdin
 
 # publish using wkg
 PROJECT_NAME="rust_wasi_hello"
-REGISTRY_REFERENCE="ghcr.io/yoshuawuyts/rust-wasi-hello:latest"
+GH_USER=$(gh api user --jq '.login')
+REGISTRY_REFERENCE="ghcr.io/${GH_USER}/rust-wasi-hello:latest"
+
 wkg oci push $REGISTRY_REFERENCE target/wasm32-wasip1/release/$PROJECT_NAME.wasm
