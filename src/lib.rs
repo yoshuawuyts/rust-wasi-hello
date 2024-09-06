5aba90aa1cc1ba8155a3114671579584aeae27ff
Yosh <github@yosh.is>
2024-09-06 14:54:29 +0200

    create routes

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use url::Url;
 pub use wasi::http::types::{
     Fields, IncomingRequest, OutgoingBody, OutgoingResponse, ResponseOutparam,
 };
@@ -6,18 +7,59 @@ struct Component;
 wasi::http::proxy::export!(Component);
 
 impl wasi::exports::http::incoming_handler::Guest for Component {
-    fn handle(_request: IncomingRequest, outparam: ResponseOutparam) {
-        let hdrs = Fields::new();
-        let resp = OutgoingResponse::new(hdrs);
-        let body = resp.body().expect("outgoing response");
-
-        ResponseOutparam::set(outparam, Ok(resp));
-
-        let out = body.write().expect("outgoing stream");
-        out.blocking_write_and_flush(b"Hello, wasi:http/proxy world!\n")
-            .expect("writing response");
-
-        drop(out);
-        OutgoingBody::finish(body, None).unwrap();
+    fn handle(req: IncomingRequest, outparam: ResponseOutparam) {
+        let url = req.path_with_query().unwrap();
+        let url = Url::parse(&url).expect("could not parse the URL");
+        http_home(req, outparam);
+        // dbg!(&url);
+        // match url.path() {
+        //     "/wait" => http_wait(req, outparam),
+        //     // "/echo" => {} // TODO
+        //     // "/host" => {} // TODO
+        //     "/" | _ => http_home(req, outparam),
+        // }
     }
+}
+
+fn http_home(_req: IncomingRequest, outparam: ResponseOutparam) {
+    let headers = Fields::new();
+    let res = OutgoingResponse::new(headers);
+    let body = res.body().expect("outgoing response");
+
+    ResponseOutparam::set(outparam, Ok(res));
+
+    let out = body.write().expect("outgoing stream");
+    out.blocking_write_and_flush(b"Hello, wasi:http/proxy world!\n")
+        .expect("writing response");
+
+    drop(out);
+    OutgoingBody::finish(body, None).unwrap();
+}
+
+fn http_wait(_req: IncomingRequest, outparam: ResponseOutparam) {
+    // Get the time now
+    let now = wasi::clocks::monotonic_clock::now();
+
+    // Sleep for 1 second
+    let nanos = 1_000_000_000;
+    let pollable = wasi::clocks::monotonic_clock::subscribe_duration(nanos);
+    pollable.block();
+
+    // Compute how long we slept for.
+    let elapsed = wasi::clocks::monotonic_clock::now() - now;
+    let elapsed = elapsed / 1_000_000; // change to millis
+
+    let headers = Fields::new();
+    let res = OutgoingResponse::new(headers);
+    let body = res.body().expect("outgoing response");
+
+    ResponseOutparam::set(outparam, Ok(res));
+
+    let out = body.write().expect("outgoing stream");
+    let msg = format!("slept for {elapsed} millis");
+    out.blocking_write_and_flush(msg.as_bytes())
+        .expect("writing response");
+
+    drop(out);
+    OutgoingBody::finish(body, None).unwrap();
 }
