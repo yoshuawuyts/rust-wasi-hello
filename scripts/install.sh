02df0cf833db877e7eeb9a15e03c5ed4949d04d4
Aaron Wislang <aaron.wislang@microsoft.com>
2024-09-19 15:17:50 -0400

    Add wasmtime, re-order + --force
    Signed-off-by: Aaron Wislang <aaron.wislang@microsoft.com>

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,9 +1,7 @@
-# setup dir
-mkdir -p tmp
-cd tmp
-
-# install wkg
-cargo install wkg
+curl https://wasmtime.dev/install.sh -sSf | bash
 
 # install cargo-component
 cargo install cargo-component --force --version 0.16.0
+
+# install wkg
+cargo install wkg --force 
