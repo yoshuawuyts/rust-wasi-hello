644fa205eb44cefff237b5bd76ea90740870375e
Aaron Wislang <aaron.wislang@microsoft.com>
2024-09-19 15:16:15 -0400

    cargo install wkg
    Signed-off-by: Aaron Wislang <aaron.wislang@microsoft.com>

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,10 +3,7 @@ mkdir -p tmp
 cd tmp
 
 # install wkg
-git clone https://github.com/bytecodealliance/wasm-pkg-tools
-cd wasm-pkg-tools/crates/wkg
-cargo install --path .
-cd ../../../../
+cargo install wkg
 
 # install cargo-component
 cargo install cargo-component --force --version 0.16.0
