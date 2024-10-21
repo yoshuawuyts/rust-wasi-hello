e24664bde6bb8969c2b336c2333ae5a8a1289950
Joshua Duffney <jduffney@microsoft.com>
2024-10-21 09:59:51 -0500

    mod: add meta tag action for versioning

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
+tags = "$1"
 # not running in github actions? use gh cli to login, etc, locally
 if [ "$GITHUB_ACTIONS" != "true" ]; then
     # login to the registry
-    # if running locally, we may need to: 
+    # if running locally, we may need to:
     # gh auth refresh -h github.com -s write:packages,read:packages
     gh auth token | docker login ghcr.io --username YOURUSERNAME --password-stdin
     GH_USER=$(gh api user --jq '.login')
@@ -11,6 +11,6 @@ if [ "$GITHUB_ACTIONS" != "true" ]; then
 fi
 
 PROJECT_NAME="rust_wasi_hello"
-REGISTRY_REFERENCE="ghcr.io/${IMAGE_NAME}:latest"
+REGISTRY_REFERENCE="ghcr.io/${IMAGE_NAME}:${tags}"
 
 wkg oci push $REGISTRY_REFERENCE target/wasm32-wasip1/release/$PROJECT_NAME.wasm
